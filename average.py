c06aa72dc21923a5675dbfc0a7f7e8e8d0bf7388
juan-paulatino <118320209+juan-paulatino@users.noreply.github.com>
2022-11-15 18:59:48 -0600

    Add files via upload

---
+++ average.py
@@ -0,0 +1,9 @@
+values = [23, 52, 59, 37, 48]
+sum = 0
+length = 0
+for value in values:
+    sum += value
+    length += 1
+
+print("Suma Total: " + str(sum) + " - Promedio: " + str(sum/length))
+print("Total Sum: " + str(sum) + " - Average: " + str(sum/length))
